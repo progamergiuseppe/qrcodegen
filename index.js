6234f0c20b69e7002cd8d67701d9e7abc543e452
progamergiuseppe <97746064+progamergiuseppe@users.noreply.github.com>
2024-07-04 10:58:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require('express');
-const axios = require('axios');
-const cors = require('cors'); // Import the cors middleware
+const router = express.Router();
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Store IP addresses of blocked users
-const blockedIPs = new Set();
-
-app.use(express.json());
 app.use(express.static('public'));
-app.use(cors({ origin: '*' })); // Allow all origins for Vercel deployments
 
-app.get("/", (req, res) => {
-    app.sendFile("")
-})
-
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+router.get('/', (req, res) => {
+    res.sendFile('GENERATEQR.html');
 });
+
+app.use('/', router);
+app.listen(3000);
